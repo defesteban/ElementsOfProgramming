7e8659b4e2222a83177a5290bba18d8f42009062
Andreeva Ekaterina <forgottenstar93@mail.ru>
2012-05-13 23:45:13 +0400

    cycle/changes

--- Прецикл/cycle.cpp
+++ Прецикл/cycle.cpp
@@ -2,71 +2,92 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <cassert>
 using namespace std;
 
-
-template <class cyc>
-class Cycle
+template <class Object>
+class Transformation
 {
-	int n;
-	cyc x0;
 public:
-	Cycle(int _n, cyc _x0)
-	{
-		n = _n; x0 = _x0;
-	}
-	cyc First();
-	cyc Second();
-private:
-	cyc my_function(cyc x, cyc n);
+	Object virtual operator() (const Object& object) const = 0;
 };
 
-template <class cyc>
-cyc Cycle<cyc>::my_function (cyc x, cyc n)
+template <class T>
+class MyTransformation : Transformation <T>
 {
-	return (x*x+37*x+7)%n;
-}
+public:
+	MyTransformation (int domain) : domain_ (domain) { }
+	T operator()(const int& x) const
+	{
+		T prom = (x*x + x) % domain_;
+		return prom;
+	}
+	int domain() const
+	{
+		return domain_;
+	}
+private:
+	int domain_;
+};
 
-template <class cyc>
-cyc Cycle<cyc>::First()
+template <class cyc, template<class> class A>
+class PreCycleLengthFinder
 {
-	int in_cycle = x0;
+	cyc x0;
+	const A<cyc>& transformation;
+public:
+	PreCycleLengthFinder(cyc _x0, const A<cyc>& _transformation) : transformation (_transformation)
+	{
+		x0 = _x0;
+	}
+	void tester(PreCycleLengthFinder prog, ofstream& oo);
+	private:
+	cyc fastPrecycleLength();
+	cyc precycleLength();
+};
+
+template <class cyc, template<class> class A>
+cyc PreCycleLengthFinder<cyc, A>::fastPrecycleLength()
+{
+	int n = transformation.domain();
+	int element_in_cycle = x0;
 
 	for (int i = 0; i < n+1; i++)
 	{
-		in_cycle = my_function(in_cycle, n);
+		element_in_cycle = transformation(element_in_cycle);
 	}
 
-	int prom = my_function(in_cycle, n);
+	int prom = transformation(element_in_cycle);
 	int numb_cycle = 1;
 
-	while (in_cycle != prom)
+	while (element_in_cycle != prom)
 	{
 		numb_cycle++;
-		prom = my_function(prom, n);
+		prom = transformation(prom);
 	}
 
 	int x = x0;
 	prom = x0;
 	for (int i = 0; i < numb_cycle; i++)
 	{
-		prom = my_function(prom, n);
+		prom = transformation(prom);
 	}
 
 	int numb_pr_cycle = 0;
 
 	while (x != prom)
 	{
-		x = my_function(x, n);
-		prom = my_function(prom, n);
+		x = transformation(x);
+		prom = transformation(prom);
 		numb_pr_cycle++;
 	}
 	return numb_pr_cycle;
 }
 
-template <class cyc>
-cyc Cycle<cyc>::Second()
+template <class cyc, template<class> class A>
+cyc PreCycleLengthFinder<cyc, A>::precycleLength()
 {
+	int n = transformation.domain();
 	vector <int> paths(n);
 	paths.assign(n, -1);
 	int beginning = x0;
@@ -75,9 +96,16 @@ cyc Cycle<cyc>::Second()
 	{
 		paths[beginning] = k;
 		k++;
-		beginning = my_function(beginning, n);
+		beginning = transformation(beginning);
 	}
 	return paths[beginning];
+}
+
+template <class cyc, template<class> class A>
+void PreCycleLengthFinder<cyc, A>::tester(PreCycleLengthFinder<cyc, A> prog, ofstream& oo)
+{
+	assert(prog.fastPrecycleLength() == prog.precycleLength());
+		oo << "Answer is " << prog.precycleLength() << endl;
 }
 
 int main()
@@ -86,16 +114,11 @@ int main()
 	ofstream oo ("output.txt");
 	int n, x0;
 	io >> n >> x0;
+		
+	MyTransformation <int> transformation(n);
+	PreCycleLengthFinder<int, MyTransformation> prog(x0, transformation);
+		
+	prog.tester(prog, oo);
 
-	Cycle<int> prog(n, x0);
-
-	if (prog.First() == prog.Second())
-	{
-		oo << "Answer is " << prog.Second() << endl;
-	}
-	else
-	{
-		oo << "Answer is not correct" << endl;
-	}
 	return 0;
 }
